3f2cf172264e1d6f3add017acb76b4ace1811dec
Sanjay Rai <sanjayharper9852@gmail.com>
2024-05-20 12:21:13 +0530

    added the dynamic search to fetch data

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,5 +1,32 @@
+import { useState } from 'react';
+
 const Home = () => {
-  return <div>Home Page</div>;
+  const [searchStr, setSearchStr] = useState('');
+
+  const onSearchValueChange = ev => {
+    setSearchStr(ev.target.value);
+  };
+
+  const onsearch = async ev => {
+    ev.preventDefault();
+
+    //const response = await fetch('https://api.github.com/users/SanjayRai98');
+
+    const response = await fetch(`https://api.github.com/users/${searchStr}`);
+    const body = await response.json();
+
+    console.log(body);
+  };
+
+  return (
+    <div>
+      <h3>Home Page</h3>
+      <form action="" onSubmit={onsearch}>
+        <input type="text" value={searchStr} onChange={onSearchValueChange} />
+        <button type="submit">Search</button>
+      </form>
+    </div>
+  );
 };
 
 export default Home;
