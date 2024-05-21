5023fdc4c063dfecca093abb6e3b2eaf4e492b1c
Sanjay Rai <sanjayharper9852@gmail.com>
2024-05-21 14:57:36 +0530

    fetch data for repos using useEffect Hooks

--- src/api/github.js
+++ src/api/github.js
@@ -1,10 +1,12 @@
 const BASE_URL = 'https://api.github.com';
 
 const apiGet = async queryString => {
-  const response = await fetch(`${BASE_URL}${queryString}`);
+  const response = await fetch(`${BASE_URL}/users${queryString}`);
   const body = await response.json();
 
   return body;
 };
 
-export const searchForUser = query => apiGet(`/users/${query}`);
+export const searchForUser = query => apiGet(`/${query}`);
+
+export const getRepoByName = loginName => apiGet(`/${loginName}/repos`);
--- src/pages/User.jsx
+++ src/pages/User.jsx
@@ -1,13 +1,51 @@
+import { useEffect, useState } from 'react';
 import { Link, useParams } from 'react-router-dom';
+import { getRepoByName } from '../api/github';
 
 const User = () => {
   const { loginName } = useParams();
 
+  const [showData, setShowData] = useState(null);
+  const [showError, setShowError] = useState(null);
+
+  useEffect(() => {
+    async function fetchData() {
+      try {
+        const data = await getRepoByName(loginName);
+        setShowData(data);
+      } catch (error) {
+        setShowError(error);
+      }
+    }
+
+    fetchData();
+  }, [loginName]);
+
+  if (showError) {
+    return <div>We have an Error : {showError.message}</div>;
+  }
+
+  if (showData) {
+    return (
+      <div>
+        <div>
+          User Page
+          <Link to="/">Search Another</Link>
+          <h1>{loginName} Repositories</h1>
+        </div>
+        <h3>Data for {showData[0].name}</h3>
+        <ul>
+          {showData.map(data => (
+            <li key={data.id}>{data.name}</li>
+          ))}
+        </ul>
+      </div>
+    );
+  }
+
   return (
     <div>
-      User Page
-      <Link to="/">Search Another</Link>
-      <h1>Showing Repository for {loginName}</h1>
+      <h1>Data is loading</h1>
     </div>
   );
 };
