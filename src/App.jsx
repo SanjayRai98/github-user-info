1bc21f7cf754b46b068e3ac0daa6dcff0ae498f4
Sanjay Rai <sanjayharper9852@gmail.com>
2024-05-22 20:13:52 +0530

    Resolve the css not loading error

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ import Starred from './pages/Starred';
 import MainLayout from './components/MainLayout';
 import User from './pages/User';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import '../src/css/style.css';
 
 const queryClient = new QueryClient();
 
