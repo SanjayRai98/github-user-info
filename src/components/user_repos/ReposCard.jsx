a46ed1f939178df313b503692f28af6bc806735d
Sanjay Rai <sanjayharper9852@gmail.com>
2024-06-27 14:09:21 +0530

    fixes minor graphics of repos card

--- src/components/user_repos/ReposCard.jsx
+++ src/components/user_repos/ReposCard.jsx
@@ -1,13 +1,18 @@
 const ReposCard = ({ name, description, createdAt }) => {
   return (
     <div className="dotted-border">
-      <h2>{name}</h2>
+      <div>
+        <h2>{name}</h2>
+      </div>
 
-      <h5>Description :</h5>
-      <p>{description}</p>
-
-      <h5>Created On :</h5>
-      <p>{createdAt}</p>
+      <div className="description">
+        <h5 className="display-inline">Description :</h5>
+        <p className="display-inline">{description}</p>
+      </div>
+      <div className="created-on">
+        <h5 className="display-inline">Created On :</h5>
+        <p className="display-inline">{createdAt}</p>
+      </div>
     </div>
   );
 };
